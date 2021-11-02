ed77b68627ff8484269dfefe7a3b20e4d7fb4e56
ihlas_sul <ihlassmada@gmail.com>
2021-11-02 08:17:11 +0800

    tugas 19 javascript
    done

---
+++ tugas19.js
@@ -0,0 +1,11 @@
+function semangat(kata){
+    return kata.repeat(3)
+}console.log(semangat('semangat...'))
+
+console.log('*'.repeat(6))
+console.log('*'.repeat(20))
+console.log('*'.repeat(40))
+console.log('*'.repeat(60))
+console.log('*'.repeat(40))
+console.log('*'.repeat(20))
+console.log('*'.repeat(6))
